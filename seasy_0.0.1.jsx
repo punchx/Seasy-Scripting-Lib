761476b56d8edbe9eedd94e0c4e68ee30633d0ed
punchx <glowdesign.md@gmail.com>
2017-07-01 21:37:00 +0300

    new version

--- seasy_0.0.1.jsx
+++ seasy_0.0.1.jsx
@@ -1,6 +1,6 @@
 ( function( global ) {
 	var Seasy = function( selector, context ) {
-		return new Seasy.prototype.sInit( selector );
+		return new Seasy.prototype.sInit( selector, context );
 	};
 	Seasy.prototype = {
 		seasy: '0.01',
@@ -26,8 +26,8 @@
 			regExpr = {
 				id: /\s*#\d+/,
 				name: /^(\^|\*)?([^:#>\+\{\}]+)/,
-				type: /:\s*(\^)?\s*([a-zA-Z]+)/,
-				attr: /(?:\{\s*(\^)?\s*([\w]+)\s*(?:=\s*(\w+))?\s*\})/
+				type: /(:)\s*([a-zA-Z]+)/,
+				attr: /(?:\{\s*(\^)?\s*([\w]+\s*(?:=\s*\w+))?\s*\})/
 			};
 		// Handle Cases for (""), (null), (undefined), (false)
 		// return non elements object
@@ -35,15 +35,21 @@
 			return this;
 		}
 		// Handle case when selector is an after effects node
-		// Return one element object[0] = AE node;			if ( isElem( selector ) ) {
+		// Return one element object[0] = AE node;			
+		if ( isElem( selector ) ) {
 			this.length = 1;
 			this[0] = this.selector = selector;
 			this.context = global; 
 			return this;
 		}
-		// Make select key for diffrent context
+
+		//Handle case when context is a layer
+		if ( isLayer( context ) && isComp( context.source ) ) {
+			context = context.source;
+		}
+		// Make select key for diffrent contexts
 		if ( !context || context === pRoot || isFolder( context ) ||
-		( isRQ( context ) || ( context == 'RQ' && context = pRoot.renderQueue ) ) {
+		( isRQ( context ) || ( context == 'RQ' && context = pRoot.renderQueue ) ) ) {
 			context = context || pRoot;
 			key = 'item';
 		} else if ( isComp( context ) ) {
@@ -67,7 +73,7 @@
 					return this;
 				}
 				if ( key = 'layer' ) {
-					Seasy.merge( this, context.slectedLayers );
+					Seasy.merge( this, context.selectedLayers );
 					this.selector = selector;
 					this.context = context;
 				}
@@ -75,6 +81,7 @@
 			//First case when number is an integer number 
 			if ( isInt( index ) ) {				
 				if ( key ) {
+					//???????????????????????????
 					elem = index > 0 ? context[key]( index ) :
 					context[key]( context['num' + key[0].toUpperCase() + key.slice(1) + 's'] + index + 1 );
 					if ( elem && isElem( elem ) ) {
@@ -97,7 +104,7 @@
 				return this;				
 			}			
 		}
-		// Handle case when selector is array
+		// Handle case when selector is an array
 		if ( isArr( selector ) ) {
 			for ( i = 0; i < selector.length; i++ ) {
 				Seasy.merge( this, Seasy( selector[i], context ) );
@@ -108,7 +115,7 @@
 		}
 		// Handle case when selector is an Object with elements: Seasy Object or AE Collection Object
 		if ( isLikeArr( selector ) ) {
-			Seasy.merge( this, selector )
+			Seasy.merge( this, selector );
 			this.selector = selector;
 			this.context = context;
 			return this;
@@ -128,42 +135,91 @@
 					return this;
 				}
 			}
-			// Match selector by +element or #id or name
-			if ( match = selctor.match( qRExpr ) ) {
+
+			if ( selector == '@' ) {
+				if ( key =='item' ) {
+					Seasy.merge( this, Seasy( 0, context ) );
+					this.selector = selector;
+					this.context = context;
+				} else if ( key == 'layer') {
+					Seasy.merge( this, Seasy( '*', context ).filter( 'active' ) );
+					this.selector = selector;
+					this.context = context;
+				}
+				return this;
+			}
+
+			if ( selector == '$' ) {
+				if ( key =='layer' ) {
+					Seasy.merge( this, Seasy( 0, context ) );
+					this.selector = selector;
+					this.context = context;
+				} else if ( key == 'item') {
+					Seasy.merge( this, Seasy( '*', context ).filter( 'selected' ) );
+					this.selector = selector;
+					this.context = context;
+				}
+				return this;
+			}
+
+
+			// Match selector by +element(create new text, camera, light... ) or #id or name
+			if ( match = selector.match( qRExpr ) ) {
 				if ( match[1] && isObj( context ) ) {
-					Seasy.merge( this, Seasy.create( match[1], context ) );
+					Seasy.merge( this, Seasy( pRoot ).new( match[1], context ) );
 					this.selector = selector;
 					this.context = context || pRoot;
 					return this;
 				} else if ( match[2] ) {
 					return Seasy( parseFloat( match[2] ), context );
 				} else ( match[4] ) {
-					Seasy.merge( this, Seasy.getByName( match[4], match[3], context ) );								
+					Seasy.merge( this, Seasy( context ).filter( match[4], match[3] ) );								
 					this.selector = selector;
 					this.context = context || pRoot;
 					return this;
 				}
 			}
-			// Try to match selector as name:type{attr = value} 
-			if (  selector.indexOf('>') == -1 && selector.indexOf(',')  == -1  ) {
-				for ( expr in regExpr ) {
-					selArr[i] = selector.match( regExpr[expr] );
-					i++;
-				}
-				if ( selArr[0] != null ) {
-					if ()
-					Seasy( parseFloat( selArr[0][1] ), context );
-				}
+			//Match selector by type (:text, :light, :camera...)
+
+			if ( match = selector.match( regExpr[type] ) ) {
+				Seasy.merge( this, context.filter( match[1], match[2] ) );
+				this.selector = selector;
+				this.context = context || pRoot;
+				return this;
+			}
+			//Match selector by attribute {attr = value}
+			if ( match = selector.match( regExpr[attr] ) ) {
+				Seasy.merge( this, context.filter( match[1] ) );
+				this.selector = selector;
+				this.context = context || pRoot;
+				return this;
 			}
 		}
 		return this;
 	}
+	Seasy.fn.extend( {
+
+		find: function( selector, inf ) { 
+				var i,
+					len = this.length,
+					myArr = [];
+				
+				for ( i = 0; i < len; i++ ) {
+					myArr[i] = Seasy( selector, this[i] );
+				}
+				Seasy.pushStack( myArr[i] );
+				this.selector = selector;
+				this.context = this;
+				return this;
+			} 
+
+	 } )
+
+	}
 	sInit.prototype = Seasy.prototype;
-	if ( global.Seasy ) {}
 	global.Seasy = global.S = Seasy;
 } )( this );
 
-console.log( typeof S() );
 
 
 
